71fbce3c821041c8f3fafb359cb5007943184eb3
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-04-19 02:40:16 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const TONAPI_BASE = process.env.TONAPI_BASE || "https://tonapi.io";
 const TONAPI_KEY = process.env.TONAPI_KEY || "";
 
