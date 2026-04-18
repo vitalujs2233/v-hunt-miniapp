687e10c945dde03fb7631e4a78863ad403014385
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-04-18 23:41:36 +0300

    Create dedust.js

---
+++ backend/services/dedust.js
@@ -0,0 +1,16 @@
+export async function getDedustQuotes() {
+  return [
+    {
+      pair: 'TON/USDT',
+      dex: 'DeDust',
+      buyPrice: 2.13,
+      sellPrice: 2.19
+    },
+    {
+      pair: 'TON/NOT',
+      dex: 'DeDust',
+      buyPrice: 0.183,
+      sellPrice: 0.188
+    }
+  ];
+}
