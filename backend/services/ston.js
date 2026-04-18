94d28191ca816f63e74e07a8271abcdf6368ae10
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-04-18 23:40:35 +0300

    Create ston.js

---
+++ backend/services/ston.js
@@ -0,0 +1,16 @@
+export async function getStonQuotes() {
+  return [
+    {
+      pair: 'TON/USDT',
+      dex: 'STON',
+      buyPrice: 2.14,
+      sellPrice: 2.18
+    },
+    {
+      pair: 'TON/NOT',
+      dex: 'STON',
+      buyPrice: 0.184,
+      sellPrice: 0.187
+    }
+  ];
+}
